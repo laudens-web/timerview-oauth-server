d91e15d815d1b014e784bbd960c5e378fa5c340d
laudens <ross.kay@laudens.com>
2026-02-09 22:50:07 +0000

    Update health.js

--- api/health.js
+++ api/health.js
@@ -1,8 +1,3 @@
-module.exports = async (req, res) => {
-  res.setHeader('Content-Type', 'application/json');
-  return res.status(200).json({
-    status: 'ok',
-    message: 'TimerView OAuth server is running',
-    timestamp: new Date().toISOString()
-  });
+module.exports = (req, res) => {
+  res.status(200).send('Hello from Vercel!');
 };
