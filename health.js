5757bf34be8053c63fdad6feb0be40202a3bed09
laudens <ross.kay@laudens.com>
2026-02-09 13:41:51 +0000

    Delete health.js

--- health.js
+++
@@ -1,8 +0,0 @@
-module.exports = async (req, res) => {
-  res.setHeader('Content-Type', 'application/json');
-  return res.status(200).json({
-    status: 'ok',
-    message: 'TimerView OAuth server is running',
-    timestamp: new Date().toISOString()
-  });
-};
